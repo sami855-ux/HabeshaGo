85dfbc58e10a5f5a20a9a57c89c282af1bb3adcb
sami <bikijoker18@gmail.com>
2025-12-10 22:52:45 +0300

    commit setting and support in user dashboard

---
+++ apps/web/app/(dashbaord)/user/settings/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SettingsPanel from "@/components/user-dashboard/SettingsPanel";
+
+export default function SettingsPage() {
+  return (
+    <div className="max-w-6xl mx-auto p-2 md:p-4">
+      <SettingsPanel />
+    </div>
+  );
+}
---
+++ apps/web/app/(dashbaord)/user/support/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SupportPanel from "@/components/user-dashboard/SupportPanel";
+
+export default function SupportPage() {
+  return (
+    <div className="max-w-6xl mx-auto p-2 md:p-4">
+      <SupportPanel />
+    </div>
+  );
+}

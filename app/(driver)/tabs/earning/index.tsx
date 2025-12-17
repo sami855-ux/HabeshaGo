f7408ef376d780ca1ba9d4c0d70484dd34084cb4
sami855-ux <samitale56@outlook.com>
2025-12-17 21:32:44 +0300

    Add EarningPage component for driver tabs

---
+++ app/(driver)/tabs/earning/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+const EarningPage = () => {
+  return (
+    <View>
+      <Text>EarningPage</Text>
+    </View>
+  )
+}
+
+export default EarningPage

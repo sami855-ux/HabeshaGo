10df671a3afbfc4fa8b39c902f52dcb7aa6ddb31
sami855-ux <samitale56@outlook.com>
2025-12-17 21:33:07 +0300

    Add TripPage component for driver tabs

---
+++ app/(driver)/tabs/trips/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+const TripPage = () => {
+  return (
+    <View>
+      <Text>TripPage</Text>
+    </View>
+  )
+}
+
+export default TripPage

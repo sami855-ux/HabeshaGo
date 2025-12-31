d2bed1a5c17c56e3c4d369c9ba309c38c8ec73a3
sami855-ux <samitale56@outlook.com>
2025-12-31 21:22:14 +0300

    updated middlewre

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,16 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-// List of protected routes
-const protectedRoutes = ["/user", "/profile", "/settings"]
-
 export function middleware(req: NextRequest) {
-  // const { pathname } = req.nextUrl
+  const token = req.cookies.get("refreshToken")?.value
 
-  // // Only run middleware on protected routes
-  // if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-  //   // Check for the session token cookie
-  //   const token = req.cookies.get("better-auth.session_token")?.value
-
-  //   if (!token) {
-  //     return NextResponse.redirect(new URL("/", req.url))
-  //   }
-
-  //   // Optionally: you could validate the token here if needed
-  //   // but usually the NestJS API will validate it
-  // }
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url))
+  }
 
   return NextResponse.next()
 }
 
-// Apply middleware only to these routes
 export const config = {
   matcher: ["/user/:path*", "/profile/:path*", "/settings/:path*"],
 }

525b30d8986cdce7e55a77dc8d5c116cc4f1eeea
Samuel T. <158570827+sami855-ux@users.noreply.github.com>
2026-01-02 23:10:10 +0300

    Export transporter from mail.js for external use

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER, // your email

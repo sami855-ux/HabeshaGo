a4a3819ac679d142fbcd9e22557c246c4781cfa4
sami855-ux <samitale56@outlook.com>
2026-01-05 22:15:57 +0300

    feat: add loggedDevice screen with slide animation

--- app/(passenger)/_layout.tsx
+++ app/(passenger)/_layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout() {
       <Stack.Screen name="search" options={{ headerShown: false }} />
       <Stack.Screen name="qrScanner" options={{ headerShown: false }} />
       <Stack.Screen name="finicialSetting" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="loggedDevice"
+        options={{ headerShown: false, animation: "slide_from_left" }}
+      />
     </Stack>
   )
 }

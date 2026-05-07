8cfbda6a339d215c208674855db1298f75416451
Samuel T. <158570827+sami855-ux@users.noreply.github.com>
2026-05-07 09:40:29 +0300

    Change axios base URL to production endpoint

--- web/services/axiosInstance.ts
+++ web/services/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://habeshago-v2.onrender.com/api",
   withCredentials: true,
 });

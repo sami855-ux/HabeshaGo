d2aa3792fd0d5f3425d9fbf5e9e4a7af7c903c60
Samuel T. <158570827+sami855-ux@users.noreply.github.com>
2026-05-07 10:15:55 +0300

    Update Resend API key in email service

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -4,7 +4,7 @@ dotenv.config()
 import { Resend } from "resend"
 import { successResponse, errorResponse } from "../utils/apiResponse.js"
 
-const resend = new Resend("re_YHfGHR6o_5XFxC378P47Fm8NqFxPrioKX");
+const resend = new Resend("re_Fsm1xGsT_2gSKwnZWB31nZ81mjhHf2iFy");
 
 export const sendEmail = async ({ to, subject, html }) => {
   try {

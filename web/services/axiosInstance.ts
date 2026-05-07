38a6e897eac04cede735b0c6c71e17a8dad303a5
Samuel T. <158570827+sami855-ux@users.noreply.github.com>
2026-05-07 10:08:29 +0300

    Update axios base URL to new endpoint

--- web/services/axiosInstance.ts
+++ web/services/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "https://habeshago-v2.onrender.com/api",
+  baseURL: "https://habeshago-pro-v1.onrender.com/api",
   withCredentials: true,
 });

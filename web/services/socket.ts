144ea2dd61b47caa6c2b5798fbd265eb84df31f6
sami855-ux <samitale56@outlook.com>
2026-05-14 11:43:55 +0300

    env varaible added

--- web/services/socket.ts
+++ web/services/socket.ts
@@ -1,14 +1,13 @@
-import { io, Socket } from "socket.io-client";
+import { io, Socket } from "socket.io-client"
 
-let socket: Socket | null = null;
+let socket: Socket | null = null
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io("http://localhost:5000", {
+    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
       transports: ["websocket"],
       autoConnect: false,
-    });
+    })
   }
-
-  return socket;
-};
+  return socket
+}

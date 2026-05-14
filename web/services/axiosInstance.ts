43cde4bf650e123465e21a7874216f59a24348ba
sami855-ux <samitale56@outlook.com>
2026-05-14 11:35:03 +0300

    env varaible added

--- web/services/axiosInstance.ts
+++ web/services/axiosInstance.ts
@@ -1,16 +1,14 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
-});
+})
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // or redux/auth
-
+  const token = localStorage.getItem("token")
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-
-  return config;
-});
+  return config
+})

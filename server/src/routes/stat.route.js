69d1a2a8d90fa20980ad4779b3b469cb661fc0ed
sami855-ux <samitale56@outlook.com>
2026-05-20 15:49:20 +0300

    commit

--- server/src/routes/stat.route.js
+++ server/src/routes/stat.route.js
@@ -17,51 +17,51 @@ const router = express.Router()
 router.get(
   "/financial-summary",
   authenticate,
-  restrictTo("ADMIN"),
+
   getFinancialSummary,
 )
 
 router.get(
   "/providers-performance",
   authenticate,
-  restrictTo("ADMIN"),
+
   getProvidersPerformance,
 )
 
 router.get(
   "/revenue-trends",
   authenticate,
-  restrictTo("ADMIN"),
+
   getRevenueTrends,
 )
 
 router.get(
   "/commission-report",
   authenticate,
-  restrictTo("ADMIN"),
+
   getCommissionReport,
 )
 router.get(
   "/commission-summary",
   authenticate,
-  restrictTo("ADMIN"),
+
   getCommissionSummary,
 )
 
 router.get(
   "/wallet-summary",
   authenticate,
-  restrictTo("ADMIN"),
+
   getWalletSummary,
 )
 router.get(
   "/wallet-earnings",
   authenticate,
-  restrictTo("ADMIN"),
+
   getWalletEarnings,
 )
-router.get("/withdrawals", authenticate, restrictTo("ADMIN"), getWithdrawals)
+router.get("/withdrawals", authenticate, getWithdrawals)
 
-router.get("/kpis", authenticate, restrictTo("ADMIN"), getKpis)
+router.get("/kpis", authenticate, getKpis)
 
 export default router

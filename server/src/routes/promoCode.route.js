6893ab2dad3e5dc65cdbf30e7ae61262039f2640
sami855-ux <samitale56@outlook.com>
2026-05-20 22:35:36 +0300

    promo code update

--- server/src/routes/promoCode.route.js
+++ server/src/routes/promoCode.route.js
@@ -11,7 +11,7 @@ import { authenticate, restrictTo } from "../middlewares/authenticate.js"
 const router = express.Router()
 
 // Apply promo code
-router.post("/apply", authenticate, restrictTo("ADMIN"), applyPromoCode)
+router.post("/apply", authenticate, applyPromoCode)
 
 // Create new promo code
 router.post("/create", authenticate, restrictTo("ADMIN"), createPromoCode)
